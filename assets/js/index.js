f4b911686ef3928e358f37e5da0d1a07526dddb0
aram1l7 <arammartirosyann16@gmail.com>
2022-05-16 16:04:18 +0400

    fix error on other pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,58 +58,60 @@ document.addEventListener('DOMContentLoaded', () => {
     username.value = '';
   }
 
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let errorMessages = [];
-    if (username.value === '' || username.value.length < 3) {
-      errorMessages.push({
-        type: 'fullname',
-        value:
-          'Username is required and should be at least 3 characters long',
-      });
-    } else {
-      username.nextElementSibling.innerText = '';
-    }
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(email.value.trim())) {
-      errorMessages.push({
-        type: 'email',
-        value: 'Email is not valid.',
-      });
-    } else {
-      email.nextElementSibling.innerText = '';
-    }
-    if (message.value === '' || message.value.length < 3) {
-      errorMessages.push({
-        type: 'message',
-        value:
-          'Message is required and should be at least 3 characters long',
-      });
-    } else {
-      message.nextElementSibling.innerText = '';
-    }
-    if (errorMessages.length > 0) {
-      return errorMessages.map((message) => {
-        let inputsDiv = document.querySelectorAll('.form-input');
-        inputsDiv.forEach((el) => {
-          let input = el.children[0];
-          if (input.id === message.type) {
-            input.nextElementSibling.innerText = message.value;
-          }
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      let errorMessages = [];
+      if (username.value === '' || username.value.length < 3) {
+        errorMessages.push({
+          type: 'fullname',
+          value:
+            'Username is required and should be at least 3 characters long',
         });
-      });
-    }
-    openSuccessModal();
-  });
-  document
-    .querySelector('.close-btn')
-    .addEventListener('click', (e) => {
-      modal.style.animation = 'fadeOut 1.5s';
-      modal.classList.add('opacity-0');
-      setTimeout(() => {
-        modal.classList.add('z-[-1]');
-        form.classList.remove('opacity-0');
-      }, 1500);
+      } else {
+        username.nextElementSibling.innerText = '';
+      }
+      const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(email.value.trim())) {
+        errorMessages.push({
+          type: 'email',
+          value: 'Email is not valid.',
+        });
+      } else {
+        email.nextElementSibling.innerText = '';
+      }
+      if (message.value === '' || message.value.length < 3) {
+        errorMessages.push({
+          type: 'message',
+          value:
+            'Message is required and should be at least 3 characters long',
+        });
+      } else {
+        message.nextElementSibling.innerText = '';
+      }
+      if (errorMessages.length > 0) {
+        return errorMessages.map((message) => {
+          let inputsDiv = document.querySelectorAll('.form-input');
+          inputsDiv.forEach((el) => {
+            let input = el.children[0];
+            if (input.id === message.type) {
+              input.nextElementSibling.innerText = message.value;
+            }
+          });
+        });
+      }
+      openSuccessModal();
     });
+    document
+      .querySelector('.close-btn')
+      .addEventListener('click', (e) => {
+        modal.style.animation = 'fadeOut 1.5s';
+        modal.classList.add('opacity-0');
+        setTimeout(() => {
+          modal.classList.add('z-[-1]');
+          form.classList.remove('opacity-0');
+        }, 1500);
+      });
+  }
 });
